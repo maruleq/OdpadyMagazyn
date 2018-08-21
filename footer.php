82bb24998f89d3d0ba6c09d88e87266005743aea
Marek Grabowski <marek@grabowscy.eu>
2018-08-21 20:01:49 +0200

    Dodanie komentarza do pliku footer.php

--- footer.php
+++ footer.php
@@ -1,3 +1,17 @@
+<?php
+
+/**
+	************************************************************************
+	**	Strona stopki aplikacji magazynowej. Projekt wykonany w Bootsrap 4
+	************************************************************************
+	**
+	**	Stopka ładuje się przy każdorazowym wyświetlaniu strony magazynu
+	**
+	************************************************************************
+*/
+
+?>
+
 		<br/>
 		<footer class="text-center text-white bg-primary site-footer">
 			<div class="container">
